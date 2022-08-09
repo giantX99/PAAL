76a26be72e3833417eef75c83a172976b42f2f62
giantX99 <93807005+giantX99@users.noreply.github.com>
2022-08-09 16:59:34 -0500

    changess

--- Intelligent Automated Elderberry Harvester/Data_Processing/resize_img.py
+++ Intelligent Automated Elderberry Harvester/Data_Processing/resize_img.py
@@ -1,14 +1,13 @@
+#crop 1280x720 image from center to 640x480 (w,h)
 from PIL import Image
 
-file_path = ''
+file_path = 'C:/Users/gian-/OneDrive/Documentos/PAAL/rgb_extracted_field_08_08_22/'
+resized_path = 'C:/Users/gian-/OneDrive/Documentos/PAAL/rgb_extracted_field_08_08_22/resized_640_480/'
 
-for i in range(10):
-    image = Image.open(file_path + '' + '.png')
+for counter in range(1, 106):
+    img_name = file_path + 'row1_rgb_' + str(counter) + '.png'
+    image = Image.open(img_name)
     cropped_pic = image.crop((160,0,1120, 720))
     new_pic = image.resize((640,480))
-    new_pic.save('resized' + i + '.png')
-    
-
-
-    
-
+    resized_name = resized_path + 'resized_row1_rgb_' + str(counter) + '.png'
+    new_pic.save(resized_name)
