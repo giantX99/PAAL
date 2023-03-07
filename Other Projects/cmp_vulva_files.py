7322121f1164d80f26e01098229fe2ea69a3d981
giantX99 <93807005+giantX99@users.noreply.github.com>
2023-03-07 16:46:04 -0600

    Update cmp_vulva_files.py

--- Other Projects/cmp_vulva_files.py
+++ Other Projects/cmp_vulva_files.py
@@ -1,9 +1,9 @@
 import shutil
 import os
 
-annotation_folder = ""
-data_folder = ""
-labeled_folder = ""
+annotation_folder = "D:/vulva_annotations/"
+data_folder = "E:/PIG_RGB_UNET/"
+labeled_folder = "E:/PIG_RGB_UNET/Labeled_Data/"
 
 annot_files = [annotation.split('__')[0] for annotation in os.listdir(annotation_folder)]
 file_names = [os.path.splitext(file)[0] for file in os.listdir(data_folder)]
@@ -15,5 +15,9 @@ for file in os.listdir(data_folder):
     file_name = os.path.splitext(file)[0]
     file_name = file_name[:-1]
     if file_name in labeled_files:
+        print("\n", file, " moved to ", labeled_folder, "\n")
         shutil.move(data_folder + file, labeled_folder + file)
+        continue
+    else:
+        print("No match for ", file)
         continue
